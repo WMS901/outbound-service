434ed7c1f761b1897b2367813805cb98efede190
chamchimayo <bazzyungfore@gmail.com>
2025-03-21 01:48:39 +0900

    edit: kafka config

--- src/config/kafka.js
+++ src/config/kafka.js
@@ -2,9 +2,20 @@ const { Kafka } = require("kafkajs");
 
 const kafka = new Kafka({
   clientId: "outbound-service",
-  brokers: ["localhost:9092"],
+  brokers: ["my-cluster-kafka-bootstrap.kafka.svc.cluster.local:9092"],
 });
 
 const consumer = kafka.consumer({ groupId: "outbound-group" });
 
-module.exports = { kafka, consumer };
+const run = async () => {
+  await consumer.connect();
+  await consumer.subscribe({ topic: "inventory-updated", fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log(`✅ Received message from Kafka: ${message.value.toString()}`);
+    },
+  });
+};
+
+run().catch(console.error);
